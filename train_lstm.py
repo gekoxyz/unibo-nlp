5757ed81aa622d26a7485d8c1e16d48a6a918209
Matteo Galiazzo <mattegaliz@gmail.com>
2025-01-25 13:01:27 +0100

    prepared for training

--- train_lstm.py
+++ train_lstm.py
@@ -1,5 +1,6 @@
+import itertools
 import pandas as pd
-
+from tqdm import tqdm
 import tensorflow as tf
 tf.random.set_seed(1337)
 
@@ -12,9 +13,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 TEST_SPLIT = 0.2
 VALIDATION_SPLIT = 0.4
 
-NUM_EPOCHS = 3
-BATCH_SIZE = 128
-REVIEW_MAX_LENGTH = 500
+TOKENIZER_MAX_WORDS = 10000
 
 hyperparameters = {
   "NUM_EPOCHS": [3, 6],
@@ -22,45 +21,53 @@ hyperparameters = {
   "REVIEW_MAX_LENGTH": [500, 1000]
 }
 
-TOKENIZER_MAX_WORDS = 10000
+def save_lstm_model(model, epochs, batch_size, review_max_length):
+  model_name = f"lstm_{epochs}_epochs_{batch_size}_batch_size_{review_max_length}_review_max_length"
+  model.save(f"./models/{model_name}.keras")
+  model.save_weights(f"./models/{model_name}.weights.h5")
 
-data = pd.read_csv("./clean_dataset.csv")
+data = pd.concat([chunk for chunk in tqdm(pd.read_csv("./clean_dataset.csv", chunksize=1000), desc="loading dataset")])
 
 train_data, test_data = train_test_split(data, test_size=TEST_SPLIT, random_state=1337, stratify=data["label"])
 
 print(f"train data shape: {train_data.shape}")
 print(f"test data shape:  {test_data.shape}")
 
-# tokenize text data
+print(f"tokenizing the test dataset")
 tokenizer = Tokenizer(num_words=TOKENIZER_MAX_WORDS)
 tokenizer.fit_on_texts(train_data["text"])
 
-X_train = pad_sequences(tokenizer.texts_to_sequences(train_data["text"]), maxlen=REVIEW_MAX_LENGTH)
-X_test = pad_sequences(tokenizer.texts_to_sequences(test_data["text"]), maxlen=REVIEW_MAX_LENGTH)
+for hyperparameters_combinations in itertools.product(*hyperparameters.values()):
+  current_hyperparameters = dict(zip(hyperparameters.keys(), hyperparameters_combinations))
+  print(f"training with: {current_hyperparameters}")
 
-Y_train = train_data["label"]
-Y_test = test_data["label"]
+  X_train = pad_sequences(tokenizer.texts_to_sequences(train_data["text"]), maxlen=current_hyperparameters["REVIEW_MAX_LENGTH"])
+  X_test = pad_sequences(tokenizer.texts_to_sequences(test_data["text"]), maxlen=current_hyperparameters["REVIEW_MAX_LENGTH"])
 
-print("nn: LSTM - Long Short-Term Memory")
-model = Sequential()
-model.add(Embedding(input_dim=TOKENIZER_MAX_WORDS, output_dim=128))
-model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
-model.add(Dense(1, activation="sigmoid"))
+  Y_train = train_data["label"]
+  Y_test = test_data["label"]
 
-print(model.summary())
+  print("nn: LSTM - Long Short-Term Memory")
+  model = Sequential()
+  model.add(Embedding(input_dim=TOKENIZER_MAX_WORDS, output_dim=128))
+  model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
+  model.add(Dense(1, activation="sigmoid"))
 
-model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+  print(model.summary())
 
-print("training the model")
-model.fit(X_train, Y_train, epochs=NUM_EPOCHS, batch_size=BATCH_SIZE, validation_split=VALIDATION_SPLIT)
+  model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
 
-def save_lstm_model(model, epochs, batch_size, review_max_length):
-  model_name = f"lstm_{epochs}_epochs_{batch_size}_batch_size_{review_max_length}_review_max_length"
-  model.save(f"./models/{model_name}.keras")
-  model.save_weights(f"./models/{model_name}.weights.h5")
+  print("training the model")
+  model.fit(X_train, Y_train, epochs=current_hyperparameters["NUM_EPOCHS"], batch_size=current_hyperparameters["BATCH_SIZE"], validation_split=VALIDATION_SPLIT)
 
-save_lstm_model(model, NUM_EPOCHS, BATCH_SIZE, REVIEW_MAX_LENGTH)
+  save_lstm_model(model, current_hyperparameters["NUM_EPOCHS"], current_hyperparameters["BATCH_SIZE"], current_hyperparameters["REVIEW_MAX_LENGTH"])
 
-loss, accuracy = model.evaluate(X_test, Y_test)
-print(f"test loss:     {loss}")
-print(f"test accuracy: {accuracy}")
+  loss, accuracy = model.evaluate(X_test, Y_test)
+  print(f"LSTM with hyperparameters {current_hyperparameters}")
+  print(f"test loss:     {loss}")
+  print(f"test accuracy: {accuracy}")
+
+  with open("hyp_lstm.txt", "a") as file:
+    file.write(f"LSTM with hyperparameters {current_hyperparameters}\n")
+    file.write(f"test loss:     {loss}\n")
+    file.write(f"test accuracy: {accuracy}\n")
